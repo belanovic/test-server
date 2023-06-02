5c496d98a1e0325472e2f73ccd02ae285d536829
Goran Belanovic <goranbelanovic@yahoo.com>
2023-06-02 02:01:46 +0200

    msg

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const config = require('config');
 const jwt = require('jsonwebtoken');
-
+/*  */
 console.log(config.get('type_of_environment'));
 /* console.log(app.get('NODE_ENV'));
-console.log(process.env.NODE_ENV); */
+console.log(process.env.NODE_ENV); */   
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
